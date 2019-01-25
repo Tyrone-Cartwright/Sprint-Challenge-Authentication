f1a1be099ae5918bd0e96eaa718417d582768315
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2019-01-25 13:18:14 -0500

    Added react to project and .env file, istalled dependencies

--- api/server.js
+++ api/server.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
 
-const configureRoutes = require('../config/routes.js');
+const configureRoutes = require("../config/routes.js");
 
 const server = express();
 
 server.use(helmet());
-server.use(cors());
+server.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 server.use(express.json());
 
 configureRoutes(server);
 
 module.exports = {
-  server,
+  server
 };
